d2c6ddc1290b3d50296202f04cb63f271394f054
Fredrik Engberg <fe@dse.se>
2014-11-15 17:22:29 +0100

    Added pulsar-shell.

---
+++ dev-tools/pulsar-shell/src/se/dse/pulsar/devtools/shell/api/package-info.java
@@ -0,0 +1,6 @@
+
+@Version("1.0.0")
+package se.dse.pulsar.devtools.shell.api;
+
+import aQute.bnd.annotation.Version;
+            
---
+++ dev-tools/pulsar-shell/src/se/dse/pulsar/devtools/shell/package-info.java
@@ -0,0 +1,6 @@
+
+@PulsarModule(symbolicName ="pulsar.devtools.shell", version="1.0.0")
+package se.dse.pulsar.devtools.shell;
+
+import se.dse.pulsar.core.api.PulsarModule;
+            
