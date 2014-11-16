76e8a2f17c0da3db547f1561a4289626533a1f13
Fredrik Engberg <fe@dse.se>
2014-11-16 17:33:53 +0100

    Added pulsar-wiki module.

---
+++ dev-tools/pulsar-wiki/src/se/dse/pulsar/devtools/wiki/api/package-info.java
@@ -0,0 +1,6 @@
+
+@Version("1.0.0")
+package se.dse.pulsar.devtools.wiki.api;
+
+import aQute.bnd.annotation.Version;
+            
---
+++ dev-tools/pulsar-wiki/src/se/dse/pulsar/devtools/wiki/package-info.java
@@ -0,0 +1,6 @@
+
+@PulsarModule(symbolicName ="pulsar.devtools.wiki", version="1.0.0")
+package se.dse.pulsar.devtools.wiki;
+
+import se.dse.pulsar.core.api.PulsarModule;
+            
